938eeffc2e0b63ffc5e7141087f3f78bfde80761
rveigarcia <rveigarcia@gmail.com>
2020-05-28 14:13:15 +0200

    código comentado

--- src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -3,9 +3,6 @@ package guru.springframework.joke.services;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by jt on 5/25/17.
- */
 @Service
 public class JokeServiceImpl implements JokeService {
 
